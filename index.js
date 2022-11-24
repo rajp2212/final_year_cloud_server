e3e400cf4670006f149dbac21610e92b318f51ac
raj <rajpawar2.rp@gmail.com>
2022-11-24 23:28:41 +0530

    make it better3

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/',(req,res)=>{
 const CONNECTION_URL='mongodb+srv://rajwardhan:221201raj@cluster0.tmeta.mongodb.net/?retryWrites=true&w=majority'
 const PORT=process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)})})
 .catch((err)=>{console.log(err)})
 
